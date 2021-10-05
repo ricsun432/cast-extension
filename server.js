a3d08b4c04751e54f4f8a098c64206c4150dcb46
Ricsun Macalindong <ricsun432>
2021-10-05 09:02:37 +0800

    feat: add home

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const app = express();
 
 app.use(express.json());
 app.use(express.static("public"));
-
+app.get("/", (req, res) => {
+  res.send("Welcome to Cast Publish Extension App");
+});
 app.post("/publish/resources/upload", async (request, response) => {
   // Ensure the "public" directory exists
   await fs.ensureDir(path.join(__dirname, "public"));
