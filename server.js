f6cc1c2bb27d3ff7e74a2dbe13c392e5c62fa435
Ricsun Macalindong <ricsun432>
2021-10-15 16:53:16 +0800

    fix: add cors

--- server.js
+++ server.js
@@ -16,15 +16,13 @@ let openConnections = [];
 const app = express();
 const Stream = new EventEmitter();
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
   );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
+  res.setHeader("Acces-Contorl-Allow-Methods", "Content-Type", "Authorization");
+  next();
 });
 app.use(
   express.json({
