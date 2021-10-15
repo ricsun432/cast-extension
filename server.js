a8799b684243dda5db10cfb79017cbb36ec68d2d
Ricsun Macalindong <ricsun432>
2021-10-15 17:30:10 +0800

    fix

--- server.js
+++ server.js
@@ -17,14 +17,14 @@ const app = express();
 const Stream = new EventEmitter();
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Request-Method", "*");
-  res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
+  // res.setHeader("Access-Control-Request-Method", "*");
+  // res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
   res.setHeader("Access-Control-Allow-Headers", "*");
-  if (req.method === "OPTIONS") {
-    res.writeHead(200);
-    res.end();
-    return;
-  }
+  // if (req.method === "OPTIONS") {
+  //   res.writeHead(200);
+  //   res.end();
+  //   return;
+  // }
   next();
 });
 app.use(
