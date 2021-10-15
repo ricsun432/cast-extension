4202e6b9a24548e403c8ae7484884cb6959f4084
Ricsun Macalindong <ricsun432>
2021-10-15 17:36:42 +0800

    fix

--- server.js
+++ server.js
@@ -15,18 +15,18 @@ let brand_, extensions_, signatures_, state_, time_, user_, code_;
 let openConnections = [];
 const app = express();
 const Stream = new EventEmitter();
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  // res.setHeader("Access-Control-Request-Method", "*");
-  // res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
-  // res.setHeader("Access-Control-Allow-Headers", "*");
-  // if (req.method === "OPTIONS") {
-  //   res.writeHead(200);
-  //   res.end();
-  //   return;
-  // }
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   // res.setHeader("Access-Control-Request-Method", "*");
+//   // res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
+//   // res.setHeader("Access-Control-Allow-Headers", "*");
+//   // if (req.method === "OPTIONS") {
+//   //   res.writeHead(200);
+//   //   res.end();
+//   //   return;
+//   // }
+//   next();
+// });
 app.use(
   express.json({
     verify: (req, res, buf) => {
