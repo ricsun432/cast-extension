d03a7b62ff4efe65d6d1b64cf5d48949b214ec98
Ricsun Macalindong <ricsun432>
2021-10-15 17:31:54 +0800

    fix

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   // res.setHeader("Access-Control-Request-Method", "*");
   // res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
-  res.setHeader("Access-Control-Allow-Headers", "*");
+  // res.setHeader("Access-Control-Allow-Headers", "*");
   // if (req.method === "OPTIONS") {
   //   res.writeHead(200);
   //   res.end();
