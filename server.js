361fea36814c5645c84fa51870932f8698397309
Ricsun Macalindong <ricsun432>
2021-10-19 15:10:48 +0800

    fix

--- server.js
+++ server.js
@@ -69,10 +69,10 @@ app.get("/env", (req, res) => {
   res.send(`${verify.isValidGetRequest(process.env.CLIENT_SECRET, req)}`);
 });
 app.get("/login", (req, res) => {
-  // if (!verify.isValidGetRequest(process.env.CLIENT_SECRET, req)) {
-  //   res.sendStatus(401);
-  //   return;
-  // }
+  if (!verify.isValidGetRequest(process.env.CLIENT_SECRET, req)) {
+    res.sendStatus(401);
+    return;
+  }
 
   const { query } = req;
   const { brand } = query; //ID of the user's team.
@@ -97,10 +97,10 @@ app.get("/login", (req, res) => {
   );
 });
 app.post("/publish/resources/upload", async (req, res) => {
-  // if (!verify.isValidPostRequest(process.env.CLIENT_SECRET, req)) {
-  //   res.sendStatus(401);
-  //   return;
-  // }
+  if (!verify.isValidPostRequest(process.env.CLIENT_SECRET, req)) {
+    res.sendStatus(401);
+    return;
+  }
   const { loggedInUsers } = db.data;
 
   //The user is logged-in
@@ -177,10 +177,10 @@ app.post("/configuration", async (req, res) => {
 });
 
 app.post("/configuration/delete", async (req, res) => {
-  // if (!verify.isValidPostRequest(process.env.CLIENT_SECRET, req)) {
-  //   res.sendStatus(401);
-  //   return;
-  // }
+  if (!verify.isValidPostRequest(process.env.CLIENT_SECRET, req)) {
+    res.sendStatus(401);
+    return;
+  }
   //Remove the current user from the database
   db.data.loggedInUsers = db.data.loggedInUsers.filter((user) => {
     return user !== req.body.user;
